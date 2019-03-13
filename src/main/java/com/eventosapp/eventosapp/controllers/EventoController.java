f955f4dbb7a444fe413622d41ad7fa66704f8eda
root <root@localhost.localdomain>
2019-03-12 21:43:01 -0300

    Inserido o model Evento

---
+++ src/main/java/com/eventosapp/eventosapp/controllers/EventoController.java
@@ -0,0 +1,13 @@
+package com.eventosapp.eventosapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+
+    @RequestMapping("/CadastraEvento")
+    public String form(){
+        return "formEvento";
+    }
+}
---
+++ src/main/java/com/eventosapp/eventosapp/models/Evento.java
@@ -0,0 +1,45 @@
+package com.eventosapp.eventosapp.models;
+
+public class Evento {
+
+    private String nome;
+    private String local;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    private String data;
+    private String horario;
+
+
+
+}
