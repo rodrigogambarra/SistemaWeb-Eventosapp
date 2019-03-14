feb3b4815e835719655911193c6b2a24108f2940
root <root@localhost.localdomain>
2019-03-14 09:39:26 -0300

    Inserida a exibição da lista de eventos na view

--- src/main/java/com/eventosapp/eventosapp/controllers/EventoController.java
+++ src/main/java/com/eventosapp/eventosapp/controllers/EventoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventoController {
@@ -23,5 +24,13 @@ public class EventoController {
     public String form(Evento evento){
         er.save(evento);
         return "redirect:/cadastrarEvento";
+    }
+
+    @RequestMapping("/eventos")
+    public ModelAndView listaEventos(){
+        ModelAndView mv = new ModelAndView("/index");//Qual página ele vai renderizar para /eventos
+        Iterable<Evento> eventos = er.findAll();
+        mv.addObject("evento", eventos);//adiciona a palavra entre {} na view;
+        return mv;
     }
 }
