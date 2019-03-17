ac9ed3f86874765e1232f8e5ec8bc25c19e81289
root <root@localhost.localdomain>
2019-03-17 19:50:40 -0300

    Inserida a lista de convidados na página de detalhes do evento

--- src/main/java/com/eventosapp/eventosapp/controllers/EventoController.java
+++ src/main/java/com/eventosapp/eventosapp/controllers/EventoController.java
@@ -45,6 +45,9 @@ public class EventoController {
         Evento evento = er.findByCodigo(codigo);
         ModelAndView mv = new ModelAndView("evento/detalhesEvento");
         mv.addObject("evento", evento);
+
+        Iterable<Convidado>convidados = cr.findByEvento(evento);
+        mv.addObject("convidados",convidados);
         System.out.println("AQUI ESTA O EVENTO: " + evento.toString());
         return mv;
     }
--- src/main/java/com/eventosapp/eventosapp/repository/ConvidadoRepository.java
+++ src/main/java/com/eventosapp/eventosapp/repository/ConvidadoRepository.java
@@ -1,8 +1,10 @@
 package com.eventosapp.eventosapp.repository;
 
 import com.eventosapp.eventosapp.models.Convidado;
+import com.eventosapp.eventosapp.models.Evento;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado, String> {
 
+    Iterable<Convidado> findByEvento(Evento evento);
 }
