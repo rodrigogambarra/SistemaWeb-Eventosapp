8ba68f80a9418d9b3d6e2861a2c334e3bd940384
root <root@localhost.localdomain>
2019-03-19 07:59:14 -0300

    Inserida a geração automática de ID_eventos

--- src/main/java/com/eventosapp/eventosapp/models/Evento.java
+++ src/main/java/com/eventosapp/eventosapp/models/Evento.java
@@ -11,7 +11,7 @@ public class Evento implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long codigo;
 
     @NotEmpty
